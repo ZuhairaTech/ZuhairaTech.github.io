3036bd7118e17d60b1a8f3c600a539ff03962268
zuhairaz <zuhairaz@heitech.com.my>
2025-03-12 14:12:52 +0800

    added logo.ico

--- js/head.js
+++ js/head.js
@@ -3,12 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     document.head.insertAdjacentHTML("beforeend", `
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Portfolio</title>
+        <title>Zuhaira Zakaria</title>
         <link rel="stylesheet" type="text/css" href="/css/form.css" />
         <link rel="stylesheet" type="text/css" href="/css/mainstyles.css" />
         <link rel="stylesheet" type="text/css" href="/css/listing.css" />
         <link rel="stylesheet" type="text/css" href="/css/form.css" />
         <link rel="stylesheet" type="text/css" href="/css/projects.css" />
+        <link rel="icon" type="image/x-icon" href="/images/logo.ico">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
         <script src="https://code.jquery.com/jquery-3.6.4.min.js" defer></script>
     `);
